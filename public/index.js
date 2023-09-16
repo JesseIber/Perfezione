f12bd50a48aa34b0bc13f96d803774583fc54126
JesseIber <jesseiber13@gmail.com>
2023-09-16 18:20:50 +0200

    test effect

--- public/index.js
+++ public/index.js
@@ -1,30 +1,20 @@
-// master GSAP and ScrollTrigger
-// https://www.creativeCodingClub.com
-// Unlock over 200 video lessons
-
-
-const races = document.querySelector(".pictures");
-console.log(races.offsetWidth)
-
-function getScrollAmount() {
-    let racesWidth = races.scrollWidth;
-    return -(racesWidth - window.innerWidth);
-}
-
-const tween = gsap.to(races, {
-    x: getScrollAmount,
-    duration: 3,
-    ease: "none",
-});
-
-
-ScrollTrigger.create({
-    trigger: ".picture_wrapper",
-    end: () => `+=${getScrollAmount() * -1}`,
-    pin: true,
-    animation: tween,
-    scrub: 1,
-    invalidateOnRefresh: true,
-    markers: true
-})
-
+window.scroll(
+    gsap.to('.hscrollLogo', {
+        xPercent: -100,
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".hscrollLogo",
+            start: "right center",
+            scrub: 1
+        }
+    }),
+    gsap.to('.hscrollPict', {
+        xPercent: 100,
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".hscrollPict",
+            start: "right center",
+            scrub: 1,
+        }
+    })
+);
